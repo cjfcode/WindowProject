7c02b8daff909b25c1dfc9684ac769445a3cc89c
HARVEY Keith D <keith.harvey@hexagon.com>
2020-08-07 11:18:51 -0500

    Working WindowChrome fixes.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Interop;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
@@ -10,34 +7,6 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            // setting a delay, even of 5 milliseconds, seems to prevent the flickering on startup caused
-            // by setting the windowChrome GlassFrameThickness and NonClientFrameEdges to non-zero values
-            // or None, respectively, and then changing them in the code.
-            Task.Delay(5).ContinueWith(_ =>
-            {
-                Dispatcher.Invoke(() =>
-                {
-                    // Setting GlassFrameThickness gives us the default shadow and
-                    // makes NonClientFrameEdges.Bottom less noticeable, while
-                    // NonClientFrameEdges.Bottom fixes a lot of issues with WindowChrome
-                    // including:
-                    //            (1) 8 pixels of empty margin when peeking a maximized window
-                    //            (2) No aero glass transparent rectangle effect on aero peek
-                    //            (3) Jittery window resizing from corners other than bottom right
-                    //                (probably because it forces the window to change position)
-                    //            (4) Blurry focus effect when selecting the minimized window from task view
-                    //
-                    mainWindowChrome.GlassFrameThickness = new Thickness(0, 0, 0, 1);
-                    mainWindowChrome.NonClientFrameEdges = System.Windows.Shell.NonClientFrameEdges.Bottom;
-                });
-            });
-
-            IntPtr hWnd = new WindowInteropHelper(this).Handle;
-            HwndSource.FromHwnd(hWnd).AddHook(new HwndSourceHook(NativeMethods.WndProc));
         }
     }
 }
---
+++ WpfApp1/WindowChromeLoadedBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shell;
+using Microsoft.Xaml.Behaviors;
+
+namespace WpfApp1
+{
+    public class WindowChromeLoadedBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += OnLoaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Loaded -= OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Task.Delay(5).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var window = Window.GetWindow(AssociatedObject);
+
+                    if (window == null) return;
+
+                    var oldWindowChrome = WindowChrome.GetWindowChrome(window);
+
+                    if (oldWindowChrome == null) return;
+
+                    var newWindowChrome = new WindowChrome
+                    {
+                        CaptionHeight = oldWindowChrome.CaptionHeight,
+                        CornerRadius = oldWindowChrome.CornerRadius,
+                        GlassFrameThickness = new Thickness(0, 0, 0, 1),
+                        NonClientFrameEdges = NonClientFrameEdges.Bottom,
+                        ResizeBorderThickness = oldWindowChrome.ResizeBorderThickness,
+                        UseAeroCaptionButtons = oldWindowChrome.UseAeroCaptionButtons
+                    };
+
+                    WindowChrome.SetWindowChrome(window, newWindowChrome);
+                });
+            });
+        }
+    }
+}
